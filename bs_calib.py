556a6727f5a30343c37f421331dce88eb08a85c5
Hope <hopelee2016@gmail.com>
2023-01-16 15:32:50 -0800

    edits

--- bs_calib.py
+++ bs_calib.py
@@ -1,12 +1,15 @@
 # import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import os
 
-folder = "bs_calibrations\\"
-path = "G:\\Shared drives\\Diamond team - Vuckovic group\\Data\\LN+diamond data\\20230113\\"
+folder = "bs calibrations/"
+path = "/mnt/g/Shared drives/Diamond team - Vuckovic group/Data/LN+diamond data/20230113/"
 
 save_title = "20230116_bs_calibration_processed"
 save_path = path
+
+save_bool = 1
 
 if save_bool == 1:
     f = open(save_path+save_title+".txt", "w")
@@ -16,15 +19,13 @@ if save_bool == 1:
 directory = path+folder
 for file in os.listdir(directory):
     full_path = directory+file
-    power = pd.read_csv(path+file_excite, skiprows=22, delimiter=';', decimal=',')
+    data = pd.read_csv(full_path, skiprows=22, delimiter=';', decimal=',')
+    power = np.asarray(data['Power (W)'])
     mean_power = np.mean(power)
     std_power = np.std(power)
-    print(file)
-    print(mean_power)
-    print(std_power)
 
     if save_bool == 1:
-        f.write(file, mean_power, std_power) 
+        f.write("{}, {}, {}/n".format(str(file), mean_power, std_power)) 
     
 if save_bool == 1:
     f.close()
