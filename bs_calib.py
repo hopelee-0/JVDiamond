73233783966fca18e2999b9f753cc57e389f43d7
Hope <hopelee2016@gmail.com>
2023-01-16 11:44:48 -0800

    edits

--- bs_calib.py
+++ bs_calib.py
@@ -11,12 +11,22 @@ save_path = path
 if save_bool == 1:
     f = open(save_path+save_title+".txt", "w")
     f.write(save_title+"\n")
+    f.write("File, mean, std\n")
 
 directory = path+folder
 for file in os.listdir(directory):
     full_path = directory+file
     power = pd.read_csv(path+file_excite, skiprows=22, delimiter=';', decimal=',')
     mean_power = np.mean(power)
+    std_power = np.std(power)
     print(file)
     print(mean_power)
+    print(std_power)
+
+    if save_bool == 1:
+        f.write(file, mean_power, std_power) 
+    
+if save_bool == 1:
+    f.close()
+
 
